d3bff1bd47a48379533a57c3d1a99030674c0ecf
Williams Hernández <hernandw@gmail.com>
2023-11-09 23:46:19 -0300

    :sparkles: logica de favorito

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,28 @@
-
-import PropTypes from 'prop-types'
-import { useContext } from 'react'
-import { MyContext } from '../context/MyContext'
+import PropTypes from "prop-types";
+import { useContext } from "react";
+import { MyContext } from "../context/MyContext";
 
 const Gallery = () => {
+  const { photos, setPhotos } = useContext(MyContext);
 
-    const { photos, setPhotos } = useContext(MyContext)
-
-   
+  const favorito = (id) => {
+    const newPhotos = photos.map((photo) => {
+      if (photo.id === id) {
+        return {
+          ...photo,
+          liked: !photo.liked,
+        };
+      }
+      return photo;
+    })
+    setPhotos(newPhotos);
+  };
   return (
-    <div className='d-flex flex-wrap gap-3'>
+    <div className="d-flex flex-wrap gap-3">
       {photos.map((photo) => (
         <div
-         
-              className='image'
+          onClick={() => favorito(photo.id)}
+          className="image"
           key={photo.id}
           style={{ backgroundImage: `url(${photo.src.landscape})` }}
         >
@@ -23,12 +32,11 @@ const Gallery = () => {
       ))}
     </div>
   );
-}
+};
 
 Gallery.propTypes = {
-    photo: PropTypes.object,
-    setPhotos: PropTypes.func
-    
-}
+  photo: PropTypes.object,
+  setPhotos: PropTypes.func,
+};
 
-export default Gallery
+export default Gallery;
--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,11 +1,43 @@
+import { useContext } from "react";
+import { MyContext } from "../context/MyContext";
 
 
 const Favourites = () => {
+
+  const { photos, setPhotos } = useContext(MyContext);
+  const favoritoFiltrado = photos.filter((photo) => photo.liked === true);
+  const favorito = (id) => {
+    const newPhotos = photos.map((photo) => {
+      if (photo.id === id) {
+        return {
+          ...photo,
+          liked: !photo.liked,
+        };
+      }
+      return photo;
+    });
+    setPhotos(newPhotos);
+  };
+
   return (
     <div>
       <h1>Favourites</h1>
+
+      <div className="d-flex flex-wrap gap-3">
+        {favoritoFiltrado.map((photo) => (
+          <div
+            onClick={() => favorito(photo.id)}
+            className="image"
+            key={photo.id}
+            style={{ backgroundImage: `url(${photo.src.landscape})` }}
+          >
+            <p>{photo.photographer}</p>
+            <p>{photo.liked ? "❤️" : "🤍"}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Favourites
